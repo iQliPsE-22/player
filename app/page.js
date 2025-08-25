27ac4773d55af6985da7b8424d5cfc771febab76
Deepak Kumar <deepaklg02@gmail.com>
2025-08-25 19:30:22 +0530

    player

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 "use client";
 import React, { useRef } from "react";
 import Player from "../components/Player";
-
+import { Video, User } from "lucide-react";
+import Image from "next/image";
 const markers = [
   { time: 5, text: "Intro", color: "#FF6900" },
   { time: 15, text: "Checkpoint 1", color: "#00d084" },
@@ -64,6 +65,32 @@ const rects = [
   },
 ];
 
+const sidebar = ["Live Camera View", "Camera Feed", "Heatmap View"];
+
+const data = [
+  {
+    name: "Prashanth",
+    id: "#1253",
+    incident: "Hardhat Missing",
+    time: "2025-08-11 16:00:34",
+    risk: "Low Risk",
+  },
+  {
+    name: "Karthik",
+    id: "#1125",
+    incident: "No Vest",
+    time: "2025-08-11 16:00:34",
+    risk: "Medium Risk",
+  },
+  {
+    name: "Prashanth",
+    id: "#1253",
+    incident: "Fall Incident",
+    time: "2025-08-11 16:00:34",
+    risk: "High Risk",
+  },
+];
+
 export default function Home() {
   const streamUrl =
     "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8";
@@ -115,14 +142,83 @@ export default function Home() {
   };
 
   return (
-    <main className="min-h-dvh flex flex-col gap-8 items-center justify-center">
-      <h1 className="text-2xl">HLS Video Player</h1>
-      <Player
-        options={videoJsOptions}
-        onReady={handlePlayerReady}
-        markers={markers}
-        rects={rects}
-      />
+    <main className="min-h-dvh flex flex-row bg-white">
+      <aside className="bg-[#f5f5f5] hidden lg:block w-fit border-r border-[#E9EAEB] overflow-y-auto hide-scrollbar">
+        <div className="pt-8 h-full w-[268px] flex flex-col gap-1 font-semibold ">
+          <div className="px-4 py-3 text-[#A4A7AE] flex flex-row gap-2 items-center">
+            <Video size={20} />
+            <h4>Monitoring</h4>
+          </div>
+          {sidebar.map((item, index) => (
+            <div
+              key={index}
+              className="pl-[42px] py-2 pr-3 text-[#535862] text-base p-4"
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+      </aside>
+      <div className="flex-1 w-full h-full bg-white px-[34px]">
+        <h4 className="font-semibold text-[24px] text-black mb-3">
+          Camera - 15 Packaging Area
+        </h4>
+        <Player
+          options={videoJsOptions}
+          onReady={handlePlayerReady}
+          markers={markers}
+          rects={rects}
+        />
+        <div
+          className="p-5 mt-3 rounded-[12px] border border-[#E9EAEB]"
+          style={{ boxShadow: "0 1px 2px 0 rgba(10, 13, 18, 0.05)" }}
+        >
+          <h4 className="font-semibold text-[20px] text-black">
+            Violation Feed
+          </h4>
+          <table className="mt-3 w-full border-collapse">
+            <tbody className="text-sm">
+              {data.map((item, index) => (
+                <tr
+                  key={index}
+                  className="border font-medium border-[#E9EAEB] rounded-[8px] bg-[#FDFDFD]"
+                >
+                  <td className="py-4 px-8 text-[#252B37] font-semibold min-w-[120px]">
+                    {item.name}
+                    <p className="text-[#535862] font-medium">ID {item.id}</p>
+                  </td>
+                  <td className="py-4 px-8 text-[#535862]">{item.incident}</td>
+                  <td className="py-4 px-8 text-[#535862]">{item.time}</td>
+                  <td
+                    className={`py-4 px-8 ${
+                      item.risk === "Low Risk"
+                        ? "text-[#F79009]"
+                        : item.risk === "Medium Risk"
+                        ? "text-[#DC6803]"
+                        : "text-[#B42318]"
+                    }`}
+                  >
+                    {item.risk}
+                  </td>
+                  <td>
+                    <button className="px-3 py-2 border border-[#D5D7DA]  rounded-[8px]">
+                      <User size={16} className="text-[#535862]" />
+                    </button>
+                    <button className="ml-4 px-3 py-2 bg-[#7D48DF] rounded-[8px] border-2">
+                      <Image
+                        src="/CircleCheck.svg"
+                        alt="Circle Check"
+                        height={16}
+                        width={16}
+                      />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </main>
   );
 }
--- components/RectCanvas.jsx
+++ components/RectCanvas.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useLayoutEffect } from "react";
-import CanvasShapes from './CanvasShapes';
+import CanvasShapes from "./CanvasShapes";
 
 export default function RectCanvas({ rects, currentTime, children }) {
   const [canvasSize, setCanvasSize] = useState({ width: 1, height: 1 });
@@ -15,11 +15,17 @@ export default function RectCanvas({ rects, currentTime, children }) {
   }, [children]);
 
   return (
-    <div ref={containerRef} className="relative w-full select-none overflow-hidden">
+    <div
+      ref={containerRef}
+      className="relative w-full select-none overflow-hidden"
+    >
       <div className="relative w-full h-full overflow-hidden border border-gray-300 rounded-lg">
-        {/* Children (video player) will be rendered here */}
         {children}
-        <CanvasShapes rects={rects} currentTime={currentTime} canvasSize={canvasSize} />
+        <CanvasShapes
+          rects={rects}
+          currentTime={currentTime}
+          canvasSize={canvasSize}
+        />
       </div>
     </div>
   );
